bb62446f88e3257903c1668749716285cc4e402d
larry-kluger <larry.kluger@docusign.com>
2018-12-27 20:47:02 +0200

    Update send_envelope.py

--- send_envelope.py
+++ send_envelope.py
@@ -1,41 +1,13 @@
 # -*- coding: utf-8 -*-
 # coding: utf-8
 import base64
-
+from os import path
 from docusign_esign import EnvelopesApi, EnvelopeDefinition, Signer, CarbonCopy, SignHere, Tabs, Recipients, Document
-
 from ds_config import DSConfig
 from ds_helper import DSHelper
 from example_base import ExampleBase
 
-ENVELOPE_1_DOCUMENT_1 = f"""
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body style="font-family:sans-serif;margin-left:2em;">
-        <h1 style="font-family: 'Trebuchet MS', Helvetica, sans-serif;
-                color: darkblue;margin-bottom: 0;">World Wide Corp</h1>
-        <h2 style="font-family: 'Trebuchet MS', Helvetica, sans-serif;
-                margin-top: 0px;margin-bottom: 3.5em;font-size: 1em;
-                color: darkblue;">Order Processing Division</h2>
-        <h4>Ordered by {DSConfig.signer_name()}</h4>
-        <p style="margin-top:0em; margin-bottom:0em;">Email:  {DSConfig.signer_email()} </p>
-        <p style="margin-top:0em; margin-bottom:0em;">Copy to: {DSConfig.cc_name()}, {DSConfig.cc_email()} </p>
-        <p style="margin-top:3em;">
-            Candy bonbon pastry jujubes lollipop wafer biscuit biscuit. Topping brownie sesame snaps
-            sweet roll pie. Croissant danish biscuit soufflé caramels jujubes jelly. Dragée danish caramels lemon
-            drops dragée. Gummi bears cupcake biscuit tiramisu sugar plum pastry.
-            Dragée gummies applicake pudding liquorice. Donut jujubes oat cake jelly-o. Dessert bear claw chocolate
-            cake gummies lollipop sugar plum ice cream gummies cheesecake.
-        </p>
-        <!-- Note the anchor tag for the signature field is in white. -->
-        <h3 style="margin-top:3em;">Agreed: <span style="color:white;">**signature_1**/</span></h3>
-    </body>
-</html>
-"""
-
+demo_docs_path = path.abspath(path.join(path.dirname(path.realpath(__file__)), 'data'))
 DOC_2_DOCX = "World_Wide_Corp_Battle_Plan_Trafalgar.docx"
 DOC_3_PDF = "World_Wide_Corp_lorem.pdf"
 
@@ -46,81 +18,132 @@ class SendEnvelope(ExampleBase):
 
     def send_envelope(self):
         self.check_token()
-        # envelope = self.create_envelope()
-        envelope = EnvelopeDefinition()
-        envelope.email_subject = "Please sign this document sent from the Python SDK"
 
-        doc1 = Document()
-        base64_content = base64.b64encode(ENVELOPE_1_DOCUMENT_1.encode()).decode('ascii')
-        doc1.document_base64 = base64_content
-        doc1.name = "Order acknowledgement"
-        doc1.file_extension = "html"
-        doc1.document_id = "1"
+        # document 1 (html) has sign here anchor tag **signature_1**
+        # document 2 (docx) has sign here anchor tag /sn1/
+        # document 3 (pdf)  has sign here anchor tag /sn1/
+        #
+        # The envelope has two recipients.
+        # recipient 1 - signer
+        # recipient 2 - cc
+        # The envelope will be sent first to the signer.
+        # After it is signed, a copy is sent to the cc person.
 
-        doc2 = Document()
-        doc2.document_base64 = base64.b64encode(DSHelper.read_content(DOC_2_DOCX))
-        doc2.name = "Battle Plan"
-        doc2.file_extension = "docx"
-        doc2.document_id = "2"
+        args = {
+            'signer_email': DSConfig.signer_email(),
+            'signer_name': DSConfig.signer_name(),
+            'cc_email': DSConfig.cc_email(),
+            'cc_name': DSConfig.cc_name(),
+        }
 
-        doc3 = Document()
-        doc3.document_base64 = base64.b64encode(DSHelper.read_content(DOC_2_DOCX))
-        doc3.name = "Lorem Ipsum"
-        doc3.file_extension = "pdf"
-        doc3.document_id = "3"
+        # create the envelope definition
+        env = EnvelopeDefinition(
+            email_subject='Please sign this document set'
+        )
+        doc1_b64 = base64.b64encode(
+            bytes(self.create_document1(args), 'utf-8')).decode('ascii')
+        # read files 2 and 3 from a local directory
+        # The reads could raise an exception if the file is not available!
+        with open(path.join(demo_docs_path, DOC_2_DOCX),
+                  "rb") as file:
+            doc2_docx_bytes = file.read()
+        doc2_b64 = base64.b64encode(doc2_docx_bytes).decode('ascii')
+        with open(path.join(demo_docs_path, DOC_3_PDF),
+                  "rb") as file:
+            doc3_pdf_bytes = file.read()
+        doc3_b64 = base64.b64encode(doc3_pdf_bytes).decode('ascii')
 
+        # Create the document models
+        document1 = Document(  # create the DocuSign document object
+            document_base64=doc1_b64,
+            name='Order acknowledgement',
+            # can be different from actual file name
+            file_extension='html',  # many different document types are accepted
+            document_id='1'  # a label used to reference the doc
+        )
+        document2 = Document(  # create the DocuSign document object
+            document_base64=doc2_b64,
+            name='Battle Plan',  # can be different from actual file name
+            file_extension='docx',  # many different document types are accepted
+            document_id='2'  # a label used to reference the doc
+        )
+        document3 = Document(  # create the DocuSign document object
+            document_base64=doc3_b64,
+            name='Lorem Ipsum',  # can be different from actual file name
+            file_extension='pdf',  # many different document types are accepted
+            document_id='3'  # a label used to reference the doc
+        )
         # The order in the docs array determines the order in the envelope
-        envelope.documents = [doc1, doc2, doc3]
-        # create a signer recipient to sign the document, identified by name and email
-        # We're setting the parameters via the object creation
-        signer1 = Signer()
-        signer1.email = DSConfig.signer_email()
-        signer1.name = DSConfig.signer_name()
-        signer1.recipient_id = "1"
-        signer1.routing_order = "1"
+        env.documents = [document1, document2, document3]
+
+        # Create the signer recipient model
+        signer1 = Signer(
+            email=args['signer_email'], name=args['signer_name'],
+            recipient_id="1", routing_order="1"
+        )
         # routingOrder (lower means earlier) determines the order of deliveries
         # to the recipients. Parallel routing order is supported by using the
         # same integer as the order for two or more recipients.
 
-        # create a cc recipient to receive a copy of the documents, identified by name and email
-        # We're setting the parameters via setters
-        cc1 = CarbonCopy()
-        cc1.email = DSConfig.cc_email()
-        cc1.name = DSConfig.cc_name()
-        cc1.routing_order = "2"
-        cc1.recipient_id = "2"
+        # create a cc recipient to receive a copy of the documents
+        cc1 = CarbonCopy(
+            email=args['cc_email'], name=args['cc_name'],
+            recipient_id="2", routing_order="2")
+
         # Create signHere fields (also known as tabs) on the documents,
         # We're using anchor (autoPlace) positioning
         #
         # The DocuSign platform searches throughout your envelope's
         # documents for matching anchor strings. So the
-        # sign_here_2 tab will be used in both document 2 and 3 since they
-        # use the same anchor string for their "signer 1" tabs.
-        sign_here1 = SignHere()
-        sign_here1.anchor_string = "**signature_1**"
-        sign_here1.anchor_units = "pixels"
-        sign_here1.anchor_x_offset = "20"
-        sign_here1.anchor_y_offset = "10"
+        # signHere2 tab will be used in both document 2 and 3 since they
+        #  use the same anchor string for their "signer 1" tabs.
+        sign_here1 = SignHere(
+            anchor_string='**signature_1**', anchor_units='pixels',
+            anchor_y_offset='10', anchor_x_offset='20')
+        sign_here2 = SignHere(
+            anchor_string='/sn1/', anchor_units='pixels',
+            anchor_y_offset='10', anchor_x_offset='20')
 
-        sign_here2 = SignHere()
-        sign_here2.anchor_string = "/sn1/"
-        sign_here2.anchor_units = "pixels"
-        sign_here2.anchor_x_offset = "20"
-        sign_here2.anchor_y_offset = "10"
-        # Tabs are set per recipient / signer
-        tabs = Tabs()
-        tabs.sign_here_tabs = [sign_here1, sign_here2]
-        signer1.tabs = tabs
+        # Add the tabs model (including the sign_here tabs) to the signer
+        # The Tabs object wants arrays of the different field/tab types
+        signer1.tabs = Tabs(sign_here_tabs=[sign_here1, sign_here2])
+
         # Add the recipients to the envelope object
-        recipients = Recipients()
-        recipients.signers = [signer1]
-        recipients.carbon_copies = [cc1]
-        envelope.recipients = recipients
+        recipients = Recipients(signers=[signer1], carbon_copies=[cc1])
+        env.recipients = recipients
+
         # Request that the envelope be sent by setting |status| to "sent".
         # To request that the envelope be created as a draft, set to "created"
-        envelope.status = "sent"
+        env.status = "sent"
 
         envelope_api = EnvelopesApi(SendEnvelope.api_client)
-        results = envelope_api.create_envelope(SendEnvelope.accountID, envelope_definition=envelope)
+        results = envelope_api.create_envelope(SendEnvelope.accountID, envelope_definition=env)
 
         return results
+
+    def create_document1(self, args):
+        """ Creates document 1 -- an html document"""
+
+        return f"""
+        <!DOCTYPE html>
+        <html>
+            <head>
+              <meta charset="UTF-8">
+            </head>
+            <body style="font-family:sans-serif;margin-left:2em;">
+            <h1 style="font-family: 'Trebuchet MS', Helvetica, sans-serif;
+                color: darkblue;margin-bottom: 0;">World Wide Corp</h1>
+            <h2 style="font-family: 'Trebuchet MS', Helvetica, sans-serif;
+              margin-top: 0px;margin-bottom: 3.5em;font-size: 1em;
+              color: darkblue;">Order Processing Division</h2>
+            <h4>Ordered by {args['signer_name']}</h4>
+            <p style="margin-top:0em; margin-bottom:0em;">Email: {args['signer_email']}</p>
+            <p style="margin-top:0em; margin-bottom:0em;">Copy to: {args['cc_name']}, {args['cc_email']}</p>
+            <p style="margin-top:3em;">
+      Candy bonbon pastry jujubes lollipop wafer biscuit biscuit. Topping brownie sesame snaps sweet roll pie. Croissant danish biscuit soufflé caramels jujubes jelly. Dragée danish caramels lemon drops dragée. Gummi bears cupcake biscuit tiramisu sugar plum pastry. Dragée gummies applicake pudding liquorice. Donut jujubes oat cake jelly-o. Dessert bear claw chocolate cake gummies lollipop sugar plum ice cream gummies cheesecake.
+            </p>
+            <!-- Note the anchor tag for the signature field is in white. -->
+            <h3 style="margin-top:3em;">Agreed: <span style="color:white;">**signature_1**/</span></h3>
+            </body>
+        </html>
+      """
